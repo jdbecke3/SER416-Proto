e67168fc87d9a34d88998f44b9c0b6aa16b242eb
Trevor Forrey <trevor4e@gmail.com>
2018-04-15 15:57:39 -0700

    volunteer form stubs working

--- src/js/volunteer.js
+++ src/js/volunteer.js
@@ -35,3 +35,25 @@ document.getElementById("submitButton").addEventListener("click", function() {
 
   document.getElementById('postSubmitMessage').innerHTML = "Thanks for Submitting!";
 });
+
+document.getElementById("submitButton1").addEventListener("click", function() {
+  let eventForm = document.getElementById('eventForm');
+  let thirdPartyForm = document.getElementById('thirdPartyForm');
+  let classForm = document.getElementById('classForm');
+  eventForm.style.display = 'none';
+  thirdPartyForm.style.display = 'none';
+  classForm.style.display = 'none';
+
+  document.getElementById('postSubmitMessage').innerHTML = "Thanks for Submitting!";
+});
+
+document.getElementById("submitButton2").addEventListener("click", function() {
+  let eventForm = document.getElementById('eventForm');
+  let thirdPartyForm = document.getElementById('thirdPartyForm');
+  let classForm = document.getElementById('classForm');
+  eventForm.style.display = 'none';
+  thirdPartyForm.style.display = 'none';
+  classForm.style.display = 'none';
+
+  document.getElementById('postSubmitMessage').innerHTML = "Thanks for Submitting!";
+});
